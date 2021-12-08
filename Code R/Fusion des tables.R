db076ce9b9619ed13e81e506c154af5a7bf22518
CharlesSIBOTTIER <90469266+CharlesSIBOTTIER@users.noreply.github.com>
2021-12-08 21:21:45 +0100

    Update Fusion des tables.R

--- Code R/Fusion des tables.R
+++ Code R/Fusion des tables.R
@@ -1,22 +1,111 @@
 library(readxl)
 library(xlsx)
+library(sqldf)
+library(dplyr)
 
-#Importation des données
+#Importation des donnÃ©es
 
-acc <- read_excel("C:/Users/Charles/OneDrive - Université de Poitiers/IRIAF/Études bilatéral/Étude statistique/Données Calyxis/ACC.xlsx")
-adu <- read_excel("C:/Users/Charles/OneDrive - Université de Poitiers/IRIAF/Études bilatéral/Étude statistique/Données Calyxis/ADU.xlsx")
-enf <- read_excel("C:/Users/Charles/OneDrive - Université de Poitiers/IRIAF/Études bilatéral/Étude statistique/Données Calyxis/ENF.xlsx")
-foy <- read_excel("C:/Users/Charles/OneDrive - Université de Poitiers/IRIAF/Études bilatéral/Étude statistique/Données Calyxis/FOY.xlsx")
-ind <- read_excel("C:/Users/Charles/OneDrive - Université de Poitiers/IRIAF/Études bilatéral/Étude statistique/Données Calyxis/IND.xlsx")
-pa <- read_excel("C:/Users/Charles/OneDrive - Université de Poitiers/IRIAF/Études bilatéral/Étude statistique/Données Calyxis/PA.xlsx")
-relier <- read_excel("C:/Users/Charles/OneDrive - Université de Poitiers/IRIAF/Études bilatéral/Étude statistique/Données Calyxis/RELINDACC.xlsx")
+acc <- read_excel("C:/Users/Charles/OneDrive - UniversitÃ© de Poitiers/IRIAF/Ã‰tudes bilatÃ©ral/Ã‰tude statistique/DonnÃ©es retravaillÃ©es/ACC.xlsx")
+adu <- read_excel("C:/Users/Charles/OneDrive - UniversitÃ© de Poitiers/IRIAF/Ã‰tudes bilatÃ©ral/Ã‰tude statistique/DonnÃ©es Calyxis/ADU.xlsx")
+enf <- read_excel("C:/Users/Charles/OneDrive - UniversitÃ© de Poitiers/IRIAF/Ã‰tudes bilatÃ©ral/Ã‰tude statistique/DonnÃ©es Calyxis/ENF.xlsx")
+foy <- read_excel("C:/Users/Charles/OneDrive - UniversitÃ© de Poitiers/IRIAF/Ã‰tudes bilatÃ©ral/Ã‰tude statistique/DonnÃ©es retravaillÃ©es/FOY.xlsx")
+ind <- read_excel("C:/Users/Charles/OneDrive - UniversitÃ© de Poitiers/IRIAF/Ã‰tudes bilatÃ©ral/Ã‰tude statistique/DonnÃ©es retravaillÃ©es/IND.xlsx")
+pa <- read_excel("C:/Users/Charles/OneDrive - UniversitÃ© de Poitiers/IRIAF/Ã‰tudes bilatÃ©ral/Ã‰tude statistique/DonnÃ©es Calyxis/PA.xlsx")
+relier <- read_excel("C:/Users/Charles/OneDrive - UniversitÃ© de Poitiers/IRIAF/Ã‰tudes bilatÃ©ral/Ã‰tude statistique/DonnÃ©es retravaillÃ©es/RELINDACC.xlsx")
 
-tableadulte<-merge(merge(merge(merge(foy,relier,by="STR_CLE"),adu,by="IND_CLE"),ind,by="IND_CLE"),acc,by="ACC_CLE")
-tableenf<-merge(merge(merge(merge(foy,relier,by="STR_CLE"),enf,by="IND_CLE"),ind,by="IND_CLE"),acc,by="ACC_CLE")
-tableage<-merge(merge(merge(merge(foy,relier,by="STR_CLE"),pa,by="IND_CLE"),ind,by="IND_CLE"),acc,by="ACC_CLE")
+#Permet d'avoir toutes les infos pour les adultes
+adulteTotal<-sqldf('SELECT *
+                   FROM ind
+                   LEFT JOIN adu ON adu.IND_CLE=ind.IND_CLE
+                   WHERE ind.IND_TYPE=2
+                   OR ind.IND_TYPE=5')
+
+#Permet de supprimer la colonne "IND_CLE" qui fait doublon
+adulteTotal<-adulteTotal[,-12]
+
+#Permet d'avoir toutes les infos pour les enfants
+enfantTotal<-sqldf('SELECT *
+                   FROM ind
+                   LEFT JOIN adu ON adu.IND_CLE=ind.IND_CLE
+                   WHERE ind.IND_TYPE=1')
+#Permet de supprimer la colonne "IND_CLE" qui fait doublon
+enfantTotal<-enfantTotal[,-12]
+
+#Permet d'avoir toutes les infos pour les PA
+PATotal<-sqldf('SELECT *
+                FROM ind
+                LEFT JOIN adu ON adu.IND_CLE=ind.IND_CLE
+                WHERE ind.IND_TYPE=3
+                OR ind.IND_TYPE=6')
+#Permet de supprimer la colonne "IND_CLE" qui fait doublon
+PATotal<-PATotal[,-12]
+
+#Permet d'avoir les personnes avec des accidents
+accident<-sqldf('SELECT relier.REF_CLE, ind.IND_CLE, relier.ACC_CLE, ind.STR_CLE
+                FROM ind
+                LEFT JOIN relier ON relier.IND_CLE=ind.IND_CLE')
+
+#Permet de supprimer les doublons
+accident<- accident %>% distinct(IND_CLE,.keep_all=TRUE)
+
+#Permet d'avoir les accidents sur les adultes
+tableAdulteACC<-sqldf('SELECT *
+                      FROM accident as acci
+                      INNER JOIN adulteTotal as adulte ON acci.IND_CLE=adulte.IND_CLE
+                      LEFT JOIN foy ON foy.STR_CLE=acci.STR_CLE
+                      WHERE acci.ACC_CLE is not null
+                   ')
+
+
+#Permet d'avoir les accidents sur les enfants
+tableEnfACC<-sqldf('SELECT *
+                    FROM accident as acci
+                    INNER JOIN enfantTotal as enfant ON acci.IND_CLE=enfant.IND_CLE
+                    LEFT JOIN foy ON foy.STR_CLE=acci.STR_CLE
+                    WHERE acci.ACC_CLE is not null
+                   ')
+
+
+#Permet d'avoir les accidents sur les personnes Ã¢gÃ©es
+tablePaACC<-sqldf('SELECT *
+                    FROM accident as acci
+                    INNER JOIN PATotal as PA ON acci.IND_CLE=PA.IND_CLE
+                    LEFT JOIN foy ON foy.STR_CLE=acci.STR_CLE
+                    WHERE acci.ACC_CLE is not null
+                   ')
+
+
+
+#Permet d'avoir les Adultes non accidentÃ©s
+tableAduNonAcc<-sqldf('SELECT *
+                      FROM accident as acci
+                      INNER JOIN adulteTotal as adulte ON acci.IND_CLE=adulte.IND_CLE
+                      LEFT JOIN foy ON foy.STR_CLE=acci.STR_CLE
+                      WHERE acci.ACC_CLE is null
+                     ')
+
+
+#Permet d'avoir les enfants non accidentÃ©s
+tableEnfNonAcc<-sqldf('SELECT *
+                      FROM accident as acci
+                      INNER JOIN enfantTotal as enfant ON acci.IND_CLE=enfant.IND_CLE
+                      LEFT JOIN foy ON foy.STR_CLE=acci.STR_CLE
+                      WHERE acci.ACC_CLE is null
+                     ')
+
+#Permet d'avoir les PA non accidentÃ©s
+tablePaNonAcc<-sqldf('SELECT *
+                      FROM accident as acci
+                      INNER JOIN PATotal as PA ON acci.IND_CLE=PA.IND_CLE
+                      LEFT JOIN foy ON foy.STR_CLE=acci.STR_CLE
+                      WHERE acci.ACC_CLE is null
+                     ')
 
 #Export en CSV
-write.csv2(tableadulte,"C:/Users/Charles/OneDrive - Université de Poitiers/IRIAF/Études bilatéral/Étude statistique/Données Calyxis/tableadulte.csv")
-write.csv2(tableenf,"C:/Users/Charles/OneDrive - Université de Poitiers/IRIAF/Études bilatéral/Étude statistique/Données Calyxis/tableenfant.csv")
-write.csv2(tableage,"C:/Users/Charles/OneDrive - Université de Poitiers/IRIAF/Études bilatéral/Étude statistique/Données Calyxis/tableâgé.csv")
+write.csv2(tableAdulteACC,"C:/Users/Charles/OneDrive - UniversitÃ© de Poitiers/IRIAF/Ã‰tudes bilatÃ©ral/Ã‰tude statistique/DonnÃ©es retravaillÃ©es/table adulte accident.csv")
+write.csv2(tableEnfACC,"C:/Users/Charles/OneDrive - UniversitÃ© de Poitiers/IRIAF/Ã‰tudes bilatÃ©ral/Ã‰tude statistique/DonnÃ©es retravaillÃ©es/table enfant accident.csv")
+write.csv2(tablePaACC,"C:/Users/Charles/OneDrive - UniversitÃ© de Poitiers/IRIAF/Ã‰tudes bilatÃ©ral/Ã‰tude statistique/DonnÃ©es retravaillÃ©es/table PA accident.csv")
 
+write.csv2(tableAduNonAcc,"C:/Users/Charles/OneDrive - UniversitÃ© de Poitiers/IRIAF/Ã‰tudes bilatÃ©ral/Ã‰tude statistique/DonnÃ©es retravaillÃ©es/table adulte sans accident.csv")
+write.csv2(tableEnfNonAcc,"C:/Users/Charles/OneDrive - UniversitÃ© de Poitiers/IRIAF/Ã‰tudes bilatÃ©ral/Ã‰tude statistique/DonnÃ©es retravaillÃ©es/table enfant sans accident.csv")
+write.csv2(tablePaNonAcc,"C:/Users/Charles/OneDrive - UniversitÃ© de Poitiers/IRIAF/Ã‰tudes bilatÃ©ral/Ã‰tude statistique/DonnÃ©es retravaillÃ©es/table PA sans accident.csv")
